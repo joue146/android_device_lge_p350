2fffab410938f5ef4be9808d2b314a97073d866e
Ron <cips173@gmail.com>
2012-07-09 17:34:19 +0530

    mass updates

--- bdaddr_read/Android.mk
+++ bdaddr_read/Android.mk
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 
-ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),p350)
+ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),pecan)
 
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
--- libcopybit/Android.mk
+++ libcopybit/Android.mk
@@ -17,21 +17,16 @@ LOCAL_PATH:= $(call my-dir)
 # HAL module implemenation, not prelinked and stored in
 # hw/<COPYPIX_HARDWARE_MODULE_ID>.<ro.board.platform>.so
 
-ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),p350)
-    include $(CLEAR_VARS)
-    ifeq ($(TARGET_GRALLOC_USES_ASHMEM),true)
-         LOCAL_CFLAGS += -DUSE_ASHMEM
-         LOCAL_CFLAGS += -DTARGET_7x27
-    endif
-
-    LOCAL_MODULE_TAGS := optional
-    LOCAL_PRELINK_MODULE := false
-    LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
-    LOCAL_SHARED_LIBRARIES := liblog
-    LOCAL_SRC_FILES := copybit.cpp
-    LOCAL_MODULE := copybit.p350
-    LOCAL_C_INCLUDES += device/lge/p350/libgralloc
-    LOCAL_CFLAGS += -DCOPYBIT_MSM7K=1
-    include $(BUILD_SHARED_LIBRARY)
+ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),pecan)
+include $(CLEAR_VARS)
+LOCAL_MODULE_TAGS := optional
+LOCAL_PRELINK_MODULE := false
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_SHARED_LIBRARIES := liblog
+LOCAL_SRC_FILES := copybit.cpp
+LOCAL_MODULE := copybit.pecan
+LOCAL_C_INCLUDES += hardware/msm7k/libgralloc
+LOCAL_CFLAGS += -DCOPYBIT_MSM7K=1
+include $(BUILD_SHARED_LIBRARY)
 endif
 
